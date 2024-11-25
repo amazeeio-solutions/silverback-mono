da1482a18ea31d697c18e1e17c69401a5a000e0f
Alex Tkachev <alex.leksat@gmail.com>
2024-11-25 20:22:33 +0400

    chore: move token-auth-middleware e2e tests to a dedicated package
    Fixed issue: On a clean project install, a first run of "playwright test" fails with
      Error: Playwright Test did not expect test() to be called here.
    The subsequent runs work.
    
    A "node_modules/.deno" folder was noticed in the stack trace. Somehow, it appears after running
    "playwright test". Usually, this should not happen. Looks like some dependency collision with
    Netlify packages.

--- packages/tests/token-auth-middleware/playwright.config.ts
+++ packages/tests/token-auth-middleware/playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm run serve',
+    command: 'pnpm --filter @amazeelabs/token-auth-middleware run serve',
     url: 'http://127.0.0.1:8887',
     timeout: 360 * 1000,
     reuseExistingServer: !process.env.CI,
