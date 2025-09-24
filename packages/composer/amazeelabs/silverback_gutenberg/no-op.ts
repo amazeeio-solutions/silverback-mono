e19eacc8f50990d7f70d6fd655a0c34004518883
Alex Tkachev <alex.leksat@gmail.com>
2025-09-24 12:50:01 +0400

    chore: remove packages moved to silverback-template

--- packages/composer/amazeelabs/silverback_gutenberg/no-op.ts
+++
@@ -1,1 +0,0 @@
-// To keep tsc calm.
--- packages/composer/amazeelabs/silverback_gutenberg/test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd ../../../../apps/silverback-drupal || exit 1
-vendor/phpunit/phpunit/phpunit web/modules/contrib/silverback_gutenberg
